46625ed743b53058bd5fc45217d62864226cfcc9
GirishDeore <58210233+GirishDeore@users.noreply.github.com>
2020-04-29 15:34:07 +0530

    Update UserController.java

--- src/main/java/com/app/controller/UserController.java
+++ src/main/java/com/app/controller/UserController.java
@@ -71,11 +71,8 @@ public ResponseEntity<String> test()
 	
 	//Integer id1=Integer.getInteger(id);
 	//List<Pdffile> listAllBooks=udao.getallbooks();
-	String str =new String("server is up and running");
-	DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-	Date date = new Date();
-	LocalDateTime today = LocalDateTime.now();
-		return new ResponseEntity<String>("server is up and running     "+today,HttpStatus.OK);
+	
+		return new ResponseEntity<String>("server is up and running     ",HttpStatus.OK);
 }
 
 
